13a9e0bd72a55e3df6c2e0a83792fbb24b97953b
evanlarsen <evan.larsen@alaskaair.com>
2016-10-10 13:59:53 -0700

    added first test

--- SeleniumBrownBag/UnitTest1.cs
+++ SeleniumBrownBag/UnitTest1.cs
@@ -22,7 +22,13 @@ namespace SeleniumBrownBag
         [TestMethod]
         public void TestMethod1()
         {
-            driver.Navigate().GoToUrl("https://google.com/");
+            driver.Navigate().GoToUrl("https://m.alaskaair.com/flightstatus/");
+            var flightNumberElem = driver.FindElement(By.CssSelector("#FlightNumber"));
+            flightNumberElem.SendKeys("454");
+            var continueBtn = driver.FindElement(By.CssSelector("input[type=submit]"));
+            continueBtn.Click();
+            var msgElem = this.driver.FindElement(By.CssSelector(".status-msg"));
+            Assert.IsTrue(msgElem.Text.Contains("Completed"), "Flight 454 should have a status of Completed");
         }
     }
 }
