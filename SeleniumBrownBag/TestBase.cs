9507377aa4801da6ff400f4bb88343c34e37c100
evanlarsen <evan.larsen@alaskaair.com>
2016-10-10 14:03:41 -0700

    adding test base

---
+++ SeleniumBrownBag/TestBase.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeleniumBrownBag
+{
+    [TestClass]
+    public abstract class TestBase
+    {
+        protected IWebDriver driver;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            string driverPath = Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location);
+            var chromeDriverService = ChromeDriverService.CreateDefaultService(driverPath);
+            driver = new ChromeDriver(chromeDriverService);
+        }
+    }
+}
--- SeleniumBrownBag/UnitTest1.cs
+++ SeleniumBrownBag/UnitTest1.cs
@@ -1,24 +1,11 @@
-﻿using System;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using OpenQA.Selenium;
-using System.IO;
-using OpenQA.Selenium.Chrome;
 
 namespace SeleniumBrownBag
 {
     [TestClass]
-    public class UnitTest1
+    public class UnitTest1 : TestBase
     {
-        IWebDriver driver;
-
-        [TestInitialize]
-        public void Initialize()
-        {
-            string driverPath = Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location);
-            var chromeDriverService = ChromeDriverService.CreateDefaultService(driverPath);
-            driver = new ChromeDriver(chromeDriverService);
-        }
-
         [TestMethod]
         public void TestMethod1()
         {
