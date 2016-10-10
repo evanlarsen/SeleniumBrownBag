7a05cbcf4c24c780e967c26c6546a724b9bcae87
evanlarsen <evan.larsen@alaskaair.com>
2016-10-10 13:44:33 -0700

    new project

---
+++ SeleniumBrownBag/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SeleniumBrownBag
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
