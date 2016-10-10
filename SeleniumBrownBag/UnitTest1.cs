a3fe6422fb3b71fd8c6f6d7e54637a7a84023e3b
evanlarsen <evan.larsen@alaskaair.com>
2016-10-10 13:54:47 -0700

    adding first test

--- SeleniumBrownBag/UnitTest1.cs
+++ SeleniumBrownBag/UnitTest1.cs
@@ -1,14 +1,28 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using System.IO;
+using OpenQA.Selenium.Chrome;
 
 namespace SeleniumBrownBag
 {
     [TestClass]
     public class UnitTest1
     {
+        IWebDriver driver;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            string driverPath = Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location);
+            var chromeDriverService = ChromeDriverService.CreateDefaultService(driverPath);
+            driver = new ChromeDriver(chromeDriverService);
+        }
+
         [TestMethod]
         public void TestMethod1()
         {
+            driver.Navigate().GoToUrl("https://google.com/");
         }
     }
 }
