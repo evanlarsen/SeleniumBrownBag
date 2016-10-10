969890de7aa5368fe3c91058958d7b00fbaac28d
evanlarsen <evan.larsen@alaskaair.com>
2016-10-10 14:14:15 -0700

    refactored to use pages

---
+++ SeleniumBrownBag/FlightInformationPage.cs
@@ -0,0 +1,24 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeleniumBrownBag
+{
+    internal class FlightInformationPage
+    {
+        readonly IWebDriver driver;
+        public FlightInformationPage(IWebDriver driver)
+        {
+            this.driver = driver;
+        }
+
+        public string GetStatusMessage()
+        {
+            var statusMsgElem = driver.FindElement(By.CssSelector(".status-msg"));
+            return statusMsgElem.Text;
+        }
+    }
+}
---
+++ SeleniumBrownBag/FlightStatusPage.cs
@@ -0,0 +1,32 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeleniumBrownBag
+{
+    internal class FlightStatusPage
+    {
+        readonly IWebDriver driver;
+        public FlightStatusPage(IWebDriver driver)
+        {
+            this.driver = driver;
+        }
+
+        public void GotoUrl()
+        {
+            driver.Navigate().GoToUrl("https://m.alaskaair.com/flightstatus");
+        }
+
+        public FlightInformationPage SearchForFlightByNumber(string flightNumber)
+        {
+            var flightNumberElem = driver.FindElement(By.CssSelector("#FlightNumber"));
+            flightNumberElem.SendKeys(flightNumber);
+            var continueBtn = driver.FindElement(By.CssSelector("input[type=submit]"));
+            continueBtn.Click();
+            return new FlightInformationPage(driver);
+        }
+    }
+}
