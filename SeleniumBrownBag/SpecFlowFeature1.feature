2887463bac5950f6e840a48e04568544dc58c73e
evanlarsen <evan.larsen@alaskaair.com>
2016-10-11 10:04:25 -0700

    adding specflow and the first test

---
+++ SeleniumBrownBag/SpecFlowFeature1.feature
@@ -0,0 +1,11 @@
+﻿Feature: SpecFlowFeature1
+	In order to avoid silly mistakes
+	As a anonymous user
+	I want to be find a flight status
+
+@mytag
+Scenario: Find flight by flight number
+	Given I am on the flight status page
+	And I have entered 454 as the flight number
+	When I press continue
+	Then the status should be Complete
---
+++ SeleniumBrownBag/SpecFlowFeature1Steps.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.IO;
+using TechTalk.SpecFlow;
+
+namespace SeleniumBrownBag
+{
+    [Binding]
+    public class SpecFlowFeature1Steps
+    {
+        IWebDriver driver;
+        FlightStatusPage flightStatusPage;
+        FlightInformationPage flightInfoPage;
+
+        [BeforeScenario]
+        public void Initialize()
+        {
+            string driverPath = Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location);
+            var chromeDriverService = ChromeDriverService.CreateDefaultService(driverPath);
+            driver = new ChromeDriver(chromeDriverService);
+            flightStatusPage = new FlightStatusPage(driver);
+        }
+
+        [Given(@"I am on the flight status page")]
+        public void GivenIAmOnTheFlightStatusPage()
+        {
+            flightStatusPage.GotoUrl();
+        }
+        
+        [Given(@"I have entered (.*) as the flight number")]
+        public void GivenIHaveEnteredAsTheFlightNumber(string flightNumber)
+        {
+            flightInfoPage = flightStatusPage.SearchForFlightByNumber(flightNumber);
+        }
+        
+        [When(@"I press continue")]
+        public void WhenIPressContinue()
+        {
+        }
+        
+        [Then(@"the status should be Complete")]
+        public void ThenTheStatusShouldBeComplete()
+        {
+            string msg = flightInfoPage.GetStatusMessage();
+            Assert.IsTrue(msg.Contains("Complete"));
+        }
+    }
+}
